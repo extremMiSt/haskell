e83875603bd65241437f40ece11254881e9083de
Michael Steinle <michael.steinle@gmail.com>
2022-10-23 18:42:20 +0200

    sheet1ex1 done

---
+++ ex01e01/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for ex01-01
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ ex01e01/app/Main.hs
@@ -0,0 +1,60 @@
+module Main where
+import Data.List (sort)
+import Test.QuickCheck ( quickCheck )
+
+-- | can work on integers, if you want a signature that only works on integers then
+    -- maxi::Integer -> Integer -> Integer
+    -- would also work. similar for the other functions
+maxi :: Ord a => a -> a -> a 
+maxi x y | x>y = x
+maxi _ y = y -- the linter suggested this, is this the origin of unused rust vars starting with _? 
+
+prop_maxi :: Integer -> Integer -> Bool -- quickcheck does not like working on generic types?
+prop_maxi x y = maxi x y == last (sort [x,y])
+
+mini :: Ord a => a -> a -> a
+mini x y | x<y = x
+mini _ y = y
+
+prop_mini :: Integer -> Integer -> Bool
+prop_mini x y = mini x y == head (sort [x,y])
+
+max3 :: Ord a => a -> a -> a -> a
+max3 x y z | x>=y,x>=z = x;
+max3 x y z | y>=x,y>=z = y;
+max3 _ _ z = z; 
+
+prop_max3 :: Integer -> Integer -> Integer -> Bool
+prop_max3 x y z = max3 x y z == last (sort [x, y, z])
+
+max3Tupled :: Ord a => (a, a, a) -> a
+max3Tupled (x,y,z) | x>=y,x>=z = x;
+max3Tupled (x,y,z) | y>=x,y>=z = y;
+max3Tupled (_,_,z)  = z;
+
+prop_max3Tupled :: (Integer, Integer, Integer)  -> Bool
+prop_max3Tupled (x,y,z) = max3Tupled (x,y,z) == last (sort [x, y, z])
+
+med :: Ord a => a -> a -> a -> a
+med x y z  | x>=y,x>=z,y>=z = y;
+med x y z  | x>=y,x>=z,y<=z = z;
+med x y z  | y>=x,y>=z,x>=z = x;
+med x y z  | y>=x,y>=z,x<=z = z;
+med x y _  | x>=y = x; -- z has to be the largest
+med _ y _  = y;
+
+prop_med :: Integer -> Integer -> Integer -> Bool
+prop_med x y z = med x y z == head (tail (sort [x, y, z]))
+
+main :: IO ()
+main = do
+    print "prop_maxi"
+    quickCheck prop_maxi
+    print "prop_mini"
+    quickCheck prop_mini
+    print "prop_max3"
+    quickCheck prop_max3
+    print "prop_max3Tupled"
+    quickCheck prop_max3Tupled
+    print "prop_med"
+    quickCheck prop_med
